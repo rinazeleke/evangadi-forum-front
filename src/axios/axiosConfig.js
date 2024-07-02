2751784b48a9ed6f3ed7b8c91ea36b78e663220f
rinazeleke <rinazeleke1994@gmail.com>
2024-07-02 00:34:47 -1200

    commit axios

--- src/axios/axiosConfig.js
+++ src/axios/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const axiosBase = axios.create({
   // baseURL: "https://evangadiforum-backend-deployement-24.onrender.com",
   // baseURL: "http://localhost:5000",
-  baseURL: "https://forumapi-utqc.onrender.com",
+  baseURL: "https://evangadiforum-backend-jivs.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
